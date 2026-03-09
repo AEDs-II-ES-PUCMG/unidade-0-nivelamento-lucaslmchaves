01095a3c2c73be998a7d4cab1eed9e8bc40ed6b9
lucaslmchaves <lucaslmchaves@gmail.com>
2026-03-09 20:46:26 -0300

    fix: corregindo o ano do criarCorretamenteAPartirDeTexto no ProdutoPerecivelTest e removendo imports não utilizados

--- test/ProdutoNaoPerecivelTest.java
+++ test/ProdutoNaoPerecivelTest.java
@@ -2,7 +2,6 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
